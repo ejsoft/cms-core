c6e5842a4d67c6160894291790d86b66cd7a22a0
ejsoft <ejsoft@mail.ru>
2017-04-26 12:53:42 +0300

    add demo theme "Default"

---
+++ protected/app/themes/Site/Default/layouts/main.php
@@ -0,0 +1,27 @@
+<?php
+/* @var $this \ej\web\View */
+/* @var $content string */
+use yii\helpers\Html;
+
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+    <div class="wrap">
+        <div class="container">
+            <?= $content ?>
+        </div>
+    </div>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
